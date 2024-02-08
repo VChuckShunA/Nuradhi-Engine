562aed99520b74b51f7d0a773776e680b4daa5dd
VChuckShunA <62978746+VChuckShunA@users.noreply.github.com>
2024-02-08 11:08:50 +0530

    Implemented Camera

--- Nuradhi Engine/first_app.cpp
+++ Nuradhi Engine/first_app.cpp
@@ -1,13 +1,17 @@
 #include "first_app.hpp"
 #include "simple_render_system.hpp"
 #include "lve_camera.hpp"
+#include "keyboard_movement_controller.hpp"
 #include <stdexcept>
 #include <array>
+#include <chrono>
+#include <cassert>
 //libs
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
+
 
 namespace lve {
 	
@@ -23,13 +27,28 @@ namespace lve {
 		SimpleRenderSystem simpleRenderSystem{ lveDevice,lveRenderer.getSwapChainRenderPass()};
 		LveCamera camera{};
 		//camera.setViewDirection(glm::vec3(0.f), glm::vec3(0.5f, 0.f, 1.f)); //down the z axis, slightly to the right
-		camera.setViewTarget(glm::vec3(-1.f, -2.f, 2.f), glm::vec3(0.f, 0.f, 2.5f)); //corresponds to the centre of the cube
+		camera.setViewTarget(glm::vec3(-1.f, -2.f, -2.f), glm::vec3(0.f, 0.f, 2.5f)); //corresponds to the centre of the cube
 		//camera.setViewTarget(glm::vec3(-1.f, -2.f, -20.f), glm::vec3(0.f, 0.f, 2.5f)); //Set the far plane to higher valeue not to be clipped at camera projection
+		
+		auto viewerObject = LveGameObject::createGameObject();
+		KeyboardMovementController cameraController{};
+
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		
 		while (!lveWindow.shouldClose()) {
 			glfwPollEvents();
+
+			auto newTime = std::chrono::high_resolution_clock::now();
+			float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
+			currentTime = newTime;
+
+			cameraController.moveInPlaneXZ(lveWindow.getGLFWwindow(), frameTime, viewerObject);
+			camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
+
 			float aspect = lveRenderer.getAspectRatio();
 			//camera.setOrthographicProjection(-aspect, aspect, -1, 1, -1, 1);
 			camera.setPerspectiveProjection(glm::radians(50.f),aspect,0.1f,10.f);
+			//camera.setOrthographicProjection(-aspect, aspect, -1, 1, 0.1f, 10.f);
 
 			//begin frame function will return a null ptr if the swap chain needs to be created
 			if (auto commandBuffer = lveRenderer.beginFrame()) {
--- Nuradhi Engine/simple_render_system.cpp
+++ Nuradhi Engine/simple_render_system.cpp
@@ -70,8 +70,8 @@ namespace lve {
 		auto projectionView = camera.getProjection() * camera.getView();
 		//update
 		for (auto& obj : gameObjects) {
-			obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.0001f, glm::two_pi<float>());
-			obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.0001f, glm::two_pi<float>());
+			//obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.0001f, glm::two_pi<float>());
+			//obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + 0.0001f, glm::two_pi<float>());
 
 			SimplePushConstantData push{};
 			push.colour = obj.colour;
